bc5ab05d9028b15ee184ec5cb11fba9e809e6d38
Squall1989 <Shev_89@mail.ru>
2023-11-23 11:04:44 +0500

    Update MultiplayerHost.cs

--- Assets/Scripts/Multiplayer/MultiplayerHost.cs
+++ Assets/Scripts/Multiplayer/MultiplayerHost.cs
@@ -1,25 +1,36 @@
 
 using MsgPck;
 using System.Collections.Generic;
+using Ulf;
 using Unity.Networking.Transport;
 
 public class MultiplayerHost 
 {
+    private SceneHost _sceneHost;
     private INetworkable _networkable;
-    private int idCount = 0;
     protected Dictionary<string, PlayerData> _players = new();
     protected Dictionary<NetworkConnection, string> _connetions = new();
 
-    public MultiplayerHost(INetworkable networkable) 
+    public MultiplayerHost(INetworkable networkable, SceneHost sceneHost) 
     {
+        _sceneHost = sceneHost;
         _networkable = networkable;
         _networkable.RegisterHandler<PlayerReadyMsg>(PlayerReady);
 
     }
 
 
-    private void PlayerReady(PlayerReadyMsg msg)
+    private async void PlayerReady(PlayerReadyMsg msg, NetworkConnection connection)
     {
-        
+        string id = msg.playerData.playerId;
+        _connetions.Add(connection, id);
+        _players.Add(id, msg.playerData);
+
+        var sceneStruct = await _sceneHost.GetSceneStruct();
+        var sceneMsg = new SceneSnapMsg()
+        { 
+             sceneStruct = sceneStruct,
+        };
+        _networkable.Send(sceneMsg, connection);
     }
 }
