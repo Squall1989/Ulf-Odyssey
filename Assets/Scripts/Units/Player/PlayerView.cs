65a7f486effe89caf71a4121584b08e2a650c7f7
Squall1989 <Shev_89@mail.ru>
2025-03-20 14:02:02 +0500

    Update HP alive logic

--- Assets/Scripts/Units/Player/PlayerView.cs
+++ Assets/Scripts/Units/Player/PlayerView.cs
@@ -36,19 +36,31 @@ namespace Ulf
 
             float bestDist = _viewDist;
             Unit bestUnit = null;
+            bool isBestAlive = false;
 
             Vector2 playerPos = _playerControl.Value.Move.Position;
 
             for (int i = 0; i < unitsOnPlanet.Count; i++)
             {
+                bool isAlive = unitsOnPlanet[i].Health.CurrHealth > 0;
+
+                if(!isAlive && isBestAlive)
+                {
+                    continue;
+                }
+
                 Vector2 unitPos = unitsOnPlanet[i].Move.Position;
+
                 if (UnitUtils.IsUnitLookTo(_playerControl.Value, unitPos))
                 {
                     float dist = (unitPos - playerPos).magnitude;
-                    if (dist < bestDist)
+
+                    if (dist < bestDist || 
+                        (isAlive && !isBestAlive && dist < _viewDist))
                     {
                         bestDist = dist;
                         bestUnit = unitsOnPlanet[i];
+                        isBestAlive = isAlive;
                     }
                 }
             }
